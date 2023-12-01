94c8f10f4311d24e141672a56191a6d5ebca207e
dhafx <dhafamaulana@webmail.umm.ac.id>
2023-12-01 19:52:28 +0700

    social_media: Tambah yg tadi Client dan account

---
+++ lib/controllers/client_controller.dart
@@ -0,0 +1,18 @@
+import 'package:get/get.dart';
+import 'package:http/http.dart';
+
+class ClientController extends GetxController {
+  Client client = Client();
+
+  @override
+  void onInit() {
+    super.onInit();
+// appwrite
+    const endPoint = "ENDPOINT_APPWRITE";
+    const projectID = "PROJECT_APPWRITE";
+    client
+        .setEndpoint(endPoint)
+        .setProject(projectID)
+        .setSelfSigned(status: true);
+  }
+}
